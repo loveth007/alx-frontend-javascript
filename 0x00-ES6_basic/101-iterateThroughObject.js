65950a164284028e4d4e183631d273f4e95f27c3
loveth007 <lovethokwudiri@gmail.com>
2023-08-03 15:44:25 +0000

    basics

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,10 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let employees = '';
-  for (const item of reportWithIterator) {
-    employees += `${item} | `;
-  }
-  return employees.slice(0, -3);
+  return reportWithIterator.join(' | ');
 }
+
+
+
+
+
+
 
